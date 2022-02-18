c7fae71fb31e2c14f2766391aecef79656f65fc5
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-02-18 15:29:50 +0530

    added fetch api and state to store data

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,17 +1,40 @@
 import React, {useState} from 'react'
-
+import MovieCard from './MovieCard'
 const SearchInput = () => {
     const [query, setQuery] = useState('')
+    const [movieData, setMovieData] = useState([])
+    console.log(movieData)
     console.log(query)
+
+    const searchMovies = async (e) => {
+        e.preventDefault();
+                
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+        
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            setMovieData(data.results);
+        }catch(err){
+            console.error(err);
+        }
+    }
+
   return (
     <main>
         <div className='input-section'>
-            <form>
+            <form onSubmit={searchMovies}>
                 <label className='input-lable' htmlFor='search'>Movie Name:</label>
                 <input className='search-input' type='text' placeholder='e.g Avengers' value={query} onChange={(e) => setQuery(e.target.value)}/>
                 <button className='search-btn' type='submit'>Search</button>
             </form>
          </div>
+
+         <div className='movie-list'>
+                {movieData && movieData.map(movie => ( 
+                    <MovieCard /> 
+                ))}
+            </div>
     </main>
   )
 }
